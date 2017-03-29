22e22b7e1b140220daa3d790aead6725f9c244e8
Julien LEICHER <jleicher@gmail.com>
2017-03-29 11:43:34 +0200

    Fix replaceBlock

--- src/PhpWord/TemplateProcessor.php
+++ src/PhpWord/TemplateProcessor.php
@@ -362,19 +362,120 @@ class TemplateProcessor
      */
     public function replaceBlock($blockname, $replacement)
     {
-        preg_match(
-            '/(<\?xml.*)(<w:p.*>\${' . $blockname . '}<\/w:.*?p>)(.*)(<w:p.*\${\/' . $blockname . '}<\/w:.*?p>)/is',
+        $matches = $this->findBlock($blockname);
+
+        if (isset($matches[1]))
+        {
+            $this->tempDocumentMainPart = str_replace
+                (
+                    $matches[0],
+                    $replacement,
+                    $this->tempDocumentMainPart
+                );
+        }
+    }
+    
+    private function findBlock($blockname)
+    {
+        // Parse the XML
+        $xml = new \SimpleXMLElement($this->tempDocumentMainPart);
+        
+        // Find the starting and ending tags
+        $startNode = false; $endNode = false;
+        foreach ($xml->xpath('//w:t') as $node)
+        {
+            if (strpos($node, '${'.$blockname.'}') !== false)
+            {
+                $startNode = $node;
+                continue;
+            }
+        
+            if (strpos($node, '${/'.$blockname.'}') !== false)
+            {
+                $endNode = $node;
+                break;
+            }
+        }
+        
+        // Make sure we found the tags
+        if ($startNode === false || $endNode === false)
+        {
+            return null;
+        }
+        
+        // Find the parent <w:p> node for the start tag
+        $node = $startNode; $startNode = null;
+        while (is_null($startNode))
+        {
+            $node = $node->xpath('..')[0];
+        
+            if ($node->getName() == 'p')
+            {
+                $startNode = $node;
+            }
+        }
+        
+        // Find the parent <w:p> node for the end tag
+        $node = $endNode; $endNode = null;
+        while (is_null($endNode))
+        {
+            $node = $node->xpath('..')[0];
+        
+            if ($node->getName() == 'p')
+            {
+                $endNode = $node;
+            }
+        }
+        
+        /*
+         * NOTE: Because SimpleXML reduces empty tags to "self-closing" tags.
+         * We need to replace the original XML with the version of XML as
+         * SimpleXML sees it. The following example should show the issue
+         * we are facing.
+         *
+         * This is the XML that my document contained orginally.
+         *
+         * ```xml
+         *  <w:p>
+         *      <w:pPr>
+         *          <w:pStyle w:val="TextBody"/>
+         *          <w:rPr></w:rPr>
+         *      </w:pPr>
+         *      <w:r>
+         *          <w:rPr></w:rPr>
+         *          <w:t>${CLONEME}</w:t>
+         *      </w:r>
+         *  </w:p>
+         * ```
+         *
+         * This is the XML that SimpleXML returns from asXml().
+         *
+         * ```xml
+         *  <w:p>
+         *      <w:pPr>
+         *          <w:pStyle w:val="TextBody"/>
+         *          <w:rPr/>
+         *      </w:pPr>
+         *      <w:r>
+         *          <w:rPr/>
+         *          <w:t>${CLONEME}</w:t>
+         *      </w:r>
+         *  </w:p>
+         * ```
+         */
+        
+        $this->tempDocumentMainPart = $xml->asXml();
+        
+        // Find the xml in between the tags
+        $xmlBlock = null;
+        preg_match
+        (
+            '/'.preg_quote($startNode->asXml(), '/').'(.*?)'.preg_quote($endNode->asXml(), '/').'/is',
             $this->tempDocumentMainPart,
             $matches
         );
-
-        if (isset($matches[3])) {
-            $this->tempDocumentMainPart = str_replace(
-                $matches[2] . $matches[3] . $matches[4],
-                $replacement,
-                $this->tempDocumentMainPart
-            );
-        }
+        
+        return $matches;
     }
 
     /**
